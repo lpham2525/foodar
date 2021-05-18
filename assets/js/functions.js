d766fe07d13c0b3dcc5dea1683f849299d94e8ba
LanChi <lpham2525@gmail.com>
2021-05-18 13:35:24 -0400

    added sumTotal function

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -9,3 +9,49 @@ const checkZipCode = () => {
 }
 
 checkZipCode()
+
+const sumTotal = () => {
+
+  let total = document.getElementById('total')
+  let expr
+
+  let classicTen = classic.checked && tenP.checked
+  let classicFif = classic.checked && fifP.checked
+  let classicTw = classic.checked && twentyP.checked
+  let premiumTen = premium.checked && tenP.checked
+  let premiumFif = premium.checked && fifP.checked
+  let premiumTw = premium.checked && twentyP.checked
+  let royalTen = royal.checked && tenP.checked
+  let royalFif = royal.checked && fifP.checked
+  let royalTw = royal.checked && twentyP.checked
+
+  switch(expr) {
+    case classicTen:
+      total.textContent="$3.30"
+      break;
+    case classicFif:
+      total.textContent = "$3.45"
+      break;
+    case classicTw:
+      total.textContent = "$3.60"
+      break;
+    case premiumTen:
+      total.textContent = "$5.50"
+      break;
+    case premiumFif:
+      total.textContent = "$5.75"
+      break;
+    case premiumTw:
+      total.textContent = "$6.00"
+      break;
+    case royalTen:
+      total.textContent = "$11.00"
+      break;
+    case royalFif:
+      total.textContent = "$11.50"
+      break;
+    case royalTw:
+      total.textContent = "$12.00"
+      break;
+  }
+}
