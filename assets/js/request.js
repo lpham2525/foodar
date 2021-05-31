9d8a21f34a1ac82cb0d65eda3cb91a1aa65621ce
LanChi <lpham2525@gmail.com>
2021-05-31 11:56:17 -0400

    added company logo to every html page

--- assets/js/request.js
+++ assets/js/request.js
@@ -1,50 +1,3 @@
-// const getTotal = (event) => {
-
-//   let total = document.getElementById('total')
-//   let expr
-
-//   let classicTen = classic.checked && tenP.checked
-//   let classicFif = classic.checked && fifP.checked
-//   let classicTw = classic.checked && twentyP.checked
-//   let premiumTen = premium.checked && tenP.checked
-//   let premiumFif = premium.checked && fifP.checked
-//   let premiumTw = premium.checked && twentyP.checked
-//   let royalTen = royal.checked && tenP.checked
-//   let royalFif = royal.checked && fifP.checked
-//   let royalTw = royal.checked && twentyP.checked
-
-//   switch (expr) {
-//     case classicTen:
-//       total.textContent = "$3.30"
-//       break;
-//     case classicFif:
-//       total.textContent = "$3.45"
-//       break;
-//     case classicTw:
-//       total.textContent = "$3.60"
-//       break;
-//     case premiumTen:
-//       total.textContent = "$5.50"
-//       break;
-//     case premiumFif:
-//       total.textContent = "$5.75"
-//       break;
-//     case premiumTw:
-//       total.textContent = "$6.00"
-//       break;
-//     case royalTen:
-//       total.textContent = "$11.00"
-//       break;
-//     case royalFif:
-//       total.textContent = "$11.50"
-//       break;
-//     case royalTw:
-//       total.textContent = "$12.00"
-//       break;
-//   }
-// }
-
-
 const calculateTotal = () => {
   let finalTotal = 0
 
@@ -69,19 +22,27 @@ const calculateTotal = () => {
 
 document.getElementById("form").addEventListener("change", calculateTotal)
 
-// const requestMade = () => {
-//   document.getElementById('request').addEventListener('click', event => {
-//     console.log('processing...')
-//     event.preventDefault()
-//     document.getElementById('requestDiv').textContent = "Request processed! Your food is on its way!"
-//     console.log('done!')
-//   })
-// }
+
 
 const requestMade = () => {
   document.getElementById('request').addEventListener('click', () => {
-    document.getElementById('requestDiv').textContent = "Request processed! Your food is on its way!"
-    console.log('done!')
+
+    let mailInput = document.getElementById('mailInput').value
+    let restaurant = document.getElementById('restaurant').value
+    const mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
+    if (mailInput !== '' && restaurant !== '' && mailInput.match(mailFormat)) {
+      document.getElementById('requestDiv').textContent = "Request processed! Your food is on its way!"
+      console.log('done!')
+    } else if (mailInput == '' || !mailInput.match(mailFormat)) {
+      console.log(mailInput)
+      document.getElementById('requestDiv').textContent = "Please enter in a valid email."
+    } else if (restaurant == '') {
+      console.log(restaurant)
+      document.getElementById('requestDiv').textContent = "Please enter in a restaurant name."
+    } else {
+      document.getElementById('requestDiv').textContent = "There was a problem processing the request."
+    }
   })
 }
 
